8e786a6c2572d80016ff37fca61650f820d07896
Gauravi-Pattiwar <gauravipattiwar04@gmail.com>
2026-06-12 00:36:54 +0530

    Almost done

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const homeBtn = document.getElementById("home-btn");
 const aboutBtn = document.getElementById("about-btn");
 const skillsBtn = document.getElementById("skills-btn");
-const projectsBtn = document.getElementById("projects-btn");
 const contactBtn = document.getElementById("contact-btn");
 const darkTheme = document.getElementById("dark-mode");
 
@@ -15,4 +14,20 @@ aboutBtn.addEventListener("click", () => {
   document
     .getElementsByClassName("about")[0]
     .scrollIntoView({ behavior: "smooth" });
+});
+
+skillsBtn.addEventListener("click", () => {
+  document
+    .getElementsByClassName("skills")[0]
+    .scrollIntoView({ behavior: "smooth" });
+});
+
+contactBtn.addEventListener("click", () => {
+  document
+    .getElementsByClassName("contact")[0]
+    .scrollIntoView({ behavior: "smooth" });
+});
+
+darkTheme.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
 });
